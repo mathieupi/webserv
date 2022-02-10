4b36d1a3b67a018a57668da1e9642069c5835948
Ledda Bryan <louchebem06@gmail.com>
2022-02-10 19:14:12 +0100

    in progress

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 11:22:10 by bledda            #+#    #+#             */
-/*   Updated: 2022/02/10 17:37:37 by bledda           ###   ########.fr       */
+/*   Updated: 2022/02/10 19:13:53 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,13 +144,13 @@ typedef struct s_config
 	string										server_name;
 	int											body_size;
 	bool										autoindex;
-	std::map<int, string>						error;
+	std::map<string, string>					error;
 
 	std::vector<string>							location;
 	std::map<string, string>					root;
 	std::map<string, string>					index_file;
-	std::map<string, string>					method;
-	std::map<string, std::map<string, string>>	redirect;
+	std::map<string, std::vector<string>>		method;
+	std::map<string, std::pair<string, string>>	redirect;
 	std::map<string, std::map<string, string>>	cgi;
 }	t_config;
 
@@ -170,7 +170,11 @@ static void print_config(t_config config)
 		webserv::debug("URL RULE" + *it);
 		webserv::debug("\tROOT : " + config.root[*it]);
 		webserv::debug("\tINDEX FILE : " + config.index_file[*it]);
-		webserv::debug("\tMETHOD ACCEPT : " + config.method[*it]);
+		webserv::debug("\tMETHOD ACCEPT : ");
+		std::vector<string> tmp_vector = config.method[*it];
+		std::vector<string>::iterator it_method = tmp_vector.begin();
+		for (; it_method != tmp_vector.end(); it_method++)
+			webserv::debug("\t\tACCEPT : " + *it_method);
 		webserv::debug("\tCGI :");
 		std::map<string, string> tmp_map = config.cgi[*it];
 		std::map<string, string>::iterator it_cgi = tmp_map.begin();
@@ -179,20 +183,84 @@ static void print_config(t_config config)
 			webserv::debug("\t\tEXTENSION FILES : " + (*it_cgi).first);
 			webserv::debug("\t\tCGI USE : " + (*it_cgi).second);
 		}
-		tmp_map = config.redirect[*it];
-		std::map<string, string>::iterator it_redirect = tmp_map.begin();
-		for (; it_redirect != tmp_map.end(); it_redirect++)
-		{
-			webserv::debug("\t\tEXTENSION FILES : " + (*it_redirect).first);
-			webserv::debug("\t\tCGI USE : " + (*it_redirect).second);
-		}
+		webserv::debug("\tREDIRECTION :");
+		std::pair<string, string> tmp_pair = config.redirect[*it];
+		webserv::debug("\t\tCODE REDIRECT : " + tmp_pair.first);
+		webserv::debug("\t\tURL REDIRECT : " + tmp_pair.second);
 	}
 }
 
-static t_config add_config(std::string conf)
+static bool isUrlKey(std::string key)
 {
-	t_config config;
+	if (key.size() <= 5)
+		return (false);
+	if ((key.substr(0, 4) == "url<" && *(key.end()-1) == '>'))
+		return (true);
+	return (false);
+}
 
+static std::string getLocation(std::string key)
+{
+	return (key.substr(4, key.size() - 5));
+}
+
+static void validKey(std::ifstream & ifs, std::string key)
+{
+	if (key != "host"
+		&& key != "port"
+		&& key != "server_name"
+		&& key != "body_size"
+		&& key != "error"
+		&& !isUrlKey(key))
+	{
+		ifs.close();
+		webserv::error("\"" + key + "\" in key, is not valid");
+		exit (1);
+	}
+}
+
+static t_config add_config(std::ifstream & ifs, std::string conf)
+{
+	t_config	config;
+	bool		find_key = false;
+	std::string tmp;
+	std::string key;
+
+	std::string::iterator it;
+	for (it = conf.begin(); it != conf.end(); it++)
+	{
+		if (*it == ':' && find_key == false)
+		{
+			std::cout << tmp << std::endl;
+			validKey(ifs, tmp);
+			find_key = true;
+			if (isUrlKey(tmp))
+				std::cout << getLocation(tmp) << std::endl;
+			key = tmp;
+			tmp.clear();
+		}
+		else if (*it == ';' && find_key == true)
+		{
+			/*
+				Reprendre ici, tu as la cle
+				tu doit parser les value et les ajouter
+			*/
+			std::cout << tmp << std::endl << std::endl;
+			find_key = false;
+			tmp.clear();
+		}
+		else
+			tmp += *it;
+	}
+
+	if (find_key)
+	{
+		ifs.close();
+		webserv::error("A parameter of one of the servers does not have a ';'");
+		exit (1);
+	}
+	
+	std::cout << std::endl;
 	print_config(config);
 	return (config);
 }
@@ -219,16 +287,12 @@ void config_file(std::string file)
 		{
 			webserv::debug(one_line_config);
 			std::cout << std::endl;
-			config.push_back(add_config(one_line_config));
+			config.push_back(add_config(ifs, one_line_config));
 			one_line_config.clear();
 			std::cout << std::endl;
 		}
 		else
-		{
-			if (!one_line_config.empty())
-				one_line_config += " ";
 			one_line_config += line;
-		}
 	}
 
 	ifs.close();
